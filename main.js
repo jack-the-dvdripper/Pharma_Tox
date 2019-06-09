40e45b8216a81deb8ad9530658f45bd0da1695ab
Jo Hausmann <35309108+jack-the-dvdripper@users.noreply.github.com>
2019-06-09 10:12:01 +0200

    Erste Version der WebApp

---
+++ main.js
@@ -0,0 +1,12 @@
+// In the main process.
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({ width: 700, height: 900 })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
